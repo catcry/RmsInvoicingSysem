f31a4152a34eadb9724f460109d48ec259ba1078
Bobby <m.babayi574@gmail.com>
2026-06-27 14:34:33 +0330

    feat: Add and Set some configs

--- Backend/Invoicing System/src/main/java/com/supptel/invoicingsystem/InvoicingSystemApplication.java
+++ Backend/Invoicing System/src/main/java/com/supptel/invoicingsystem/InvoicingSystemApplication.java
@@ -3,11 +3,16 @@ package com.supptel.invoicingsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.supptel.invoicingsystem.repository")
+@ComponentScan({"com.supptel.invoicingsystem.repository", "com.supptel.invoicingsystem.*"})
 public class InvoicingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InvoicingSystemApplication.class, args);
     }
-
 }
