336109507f3519c5f02a98acb443fd3dcf86f987
Bobby <m.babayi574@gmail.com>
2026-06-27 12:52:18 +0330

    Backend Init

---
+++ Backend/RmsInvoicingSysem/src/main/java/com/supptel/rmsinvoicingsysem/RmsInvoicingSysemApplication.java
@@ -0,0 +1,13 @@
+package com.supptel.rmsinvoicingsysem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsInvoicingSysemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmsInvoicingSysemApplication.class, args);
+    }
+
+}
---
+++ Backend/RmsInvoicingSysem/src/test/java/com/supptel/rmsinvoicingsysem/RmsInvoicingSysemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supptel.rmsinvoicingsysem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsInvoicingSysemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
