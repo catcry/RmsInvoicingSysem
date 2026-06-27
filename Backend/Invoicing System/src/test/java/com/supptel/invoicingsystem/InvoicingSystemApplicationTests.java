f30d6a20986e0834c7b8913e7e4d4c0781efbba3
Bobby <m.babayi574@gmail.com>
2026-06-27 17:04:25 +0330

    feat: Generate `Asciidoctor` Docs

--- Backend/Invoicing System/src/test/java/com/supptel/invoicingsystem/InvoicingSystemApplicationTests.java
+++ Backend/Invoicing System/src/test/java/com/supptel/invoicingsystem/InvoicingSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.supptel.invoicingsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("development")
 class InvoicingSystemApplicationTests {
 
     @Test
