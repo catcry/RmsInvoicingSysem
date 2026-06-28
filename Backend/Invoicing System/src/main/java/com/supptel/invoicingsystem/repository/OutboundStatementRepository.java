40934d791c98ee6ffc3588808b808dee95fbe6e6
Bobby <m.babayi574@gmail.com>
2026-06-28 16:35:26 +0330

    fix: Fix procedure params missmatch problem.

--- Backend/Invoicing System/src/main/java/com/supptel/invoicingsystem/repository/OutboundStatementRepository.java
+++ Backend/Invoicing System/src/main/java/com/supptel/invoicingsystem/repository/OutboundStatementRepository.java
@@ -4,6 +4,7 @@ import com.supptel.invoicingsystem.entity.OutboundStatementEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,12 @@ import java.util.List;
 @Repository
 public interface OutboundStatementRepository extends JpaRepository<OutboundStatementEntity, Long>, JpaSpecificationExecutor<OutboundStatementEntity> {
 
-    @Procedure(name = "create_update_outbound_statements_year_mon", value = "create_update_outbound_statements_year_mon")
-    void createUpdateOutboundStatements(String year, String month, String fileType);
+    @Procedure(procedureName = "create_update_outbound_statements_year_mon")
+    void createUpdateOutboundStatements(
+            @Param("i_seq_cts_year") String year,
+            @Param("i_seq_cts_mon") String month,
+            @Param("i_file_type") String fileType
+    );
 
     @Procedure(name = "update_criteria_of_outbound_stmnt", value = "update_criteria_of_outbound_stmnt")
     void updateOutboundCriteria(Integer outboundId, String excluded, String included);
